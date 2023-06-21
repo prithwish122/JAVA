fbf9e96db96f46aa09205e951fdcea18abf5ede6
Prithwish Chatterjee <116712239+prithwish122@users.noreply.github.com>
2023-06-21 20:35:02 +0530

    Create func.java

---
+++ func.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class calculator{
+    public int add(int num1,int num2)
+    {
+        int result;
+        result = num1+num2;
+        return result;
+    }
+}
+
+
+public class func{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter two numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        calculator calc=new calculator();
+        int res;
+        res=calc.add(a,b);
+        System.out.println("The sum of the two numbers is :"+res);
+
+
+    }
+    
+}
